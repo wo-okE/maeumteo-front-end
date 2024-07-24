0c844f68152887a58c79a313d583feb9d1458792
SeungWook <seugn97@gmail.com>
2024-07-24 22:53:54 +0900

    상품판매 페이지

--- src/SellProducts.js
+++ src/SellProducts.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function SellProducts(){
-    return (<div>123</div>);
+    const [uploadImgUrl, setUploadImgUrl] = useState("");
+
+  const onchangeImageUpload = (e)=> {
+     const {files} = e.target;
+     const uploadFile = files[0];
+     const reader = new FileReader();
+     reader.readAsDataURL(uploadFile);
+     reader.onloadend = ()=> {
+     setUploadImgUrl(reader.result);
+  }
+}
+    return (
+    <>
+        <img src = {uploadImgUrl} img = "img"/>
+        <input type = "file" accept = "image/*" onChange = {onchangeImageUpload}/>
+    </>
+    );
 }
 
 export default SellProducts;
